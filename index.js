498f3c7ba1d9ba24a36bfcd16b1730f06fdb5d72
NyaradzoUBere <nybe5598@colorado.edu>
2020-09-27 12:57:46 -0600

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-function findMinAndRemove(array){
+let unsortedArray = [5, 6, -1, 1, 3];
 
+
+function findMinAndRemove(array) {
+  let min = array[0]
+  let minIndex = 0
+  for (let i = 0; i < array.length ; i++) {
+    if (array[i] < min) {
+      min = array[i]
+      minIndex = i
+    }
+  }
+  array.splice(minIndex, 1)
+  return min
+  // console.log(unsortedArray)
 }
 
-function selectionSort(array){
+// minAndRemove(unsortedArray)
 
+function selectionSort(array) {
+  let newMin
+  let sorted= []
+  while (array.length !== 0 ) {
+    newMin = findMinAndRemove(array)
+    sorted.push(newMin)
+  }
+  return sorted
 }
+
+selectionSort(unsortedArray)
